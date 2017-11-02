d0b361ad52c880d18b5fca04207ef651f05cae38
sammymwangangi <sammymwinzi2@gmail.com>
2017-11-02 15:21:09 +0300

    Updated settings

--- coolapp/settings.py
+++ coolapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^c@*xk6xi5ei#1)ye@er@mthbk^^ephvk^p0mv=&j66ntb1ke*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]','http://sammymwangangi.pythonanywhere.com/']
 
 
 # Application definition
